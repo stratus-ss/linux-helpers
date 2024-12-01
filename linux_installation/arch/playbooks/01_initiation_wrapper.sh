16e378671c2ff77104dad981fa76761fd7d77fa5
stratus-ss <steve.ovens@x86innovations.com>
2024-12-01 12:46:17 -0600

    added gaming_platform variable to install steam and lutris

--- linux_installation/arch/playbooks/01_initiation_wrapper.sh
+++ linux_installation/arch/playbooks/01_initiation_wrapper.sh
@@ -1,4 +1,4 @@
 mount -o remount,size=2G /run/archiso/cowspace
 mount -t nfs 192.168.1.1:/storage/backups/pacman_cache /var/cache/pacman/pkg
 pacman -Sy ansible --noconfirm
-time ansible-playbook -v stage1.yaml
+time ansible-playbook -v tasks/stage1.yaml
